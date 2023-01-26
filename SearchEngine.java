4eb055233a3398cc3e2744e6340c1f2f4132179d
juicebox013 <justin.nguyen335@gmail.com>
2023-01-26 14:14:45 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    public String searchHandler(String url) {
+        if(url.getPath().equals("/")) {
+            
+        }
+    }
+}
+
+class SearchEngine {
+
+}
